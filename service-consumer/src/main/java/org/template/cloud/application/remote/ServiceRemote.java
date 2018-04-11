8643e2c50909c4604a17219a7e0b34b18b50469a
liaoyuecai <33653091+liaoyuecai@users.noreply.github.com>
2018-04-11 17:51:27 +0800

    Create ServiceRemote.java

---
+++ service-consumer/src/main/java/org/template/cloud/application/remote/ServiceRemote.java
@@ -0,0 +1,12 @@
+package org.template.cloud.application.remote;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "service")
+public interface ServiceRemote {
+
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+}
