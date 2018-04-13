cb523337ca8b16ab58ed407f9fe6fc89cbdbf99c
liaoyuecai <liaoyuecai@163.com>
2018-04-13 14:50:20 +0800

    test

--- service-producer/src/test/java/org.template.cloud.service.test/MysqlTest.java
+++ service-producer/src/test/java/org.template.cloud.service.test/MysqlTest.java
@@ -1,45 +1,45 @@
-//package org.template.cloud.service.test;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.template.cloud.service.ServiceApplication;
-//import org.template.cloud.service.domain.bean.mysql.User;
-//import org.template.cloud.service.service.mysql.UserService;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = ServiceApplication.class)
-//public class MysqlTest {
-//    @Autowired
-//    UserService userService;
-//
-//    public void userTest() {
-//        User user = new User();
-//        user.setUserName("admin");
-//        user.setPassword("123456");
-//        userService.insert(user);
-//        List<User> list = userService.findAll();
-//        System.out.println(list.size());
-//    }
-//
-//    @Test
-//    public void insertList() {
-//        List<User> list = new ArrayList();
-//        User user;
-//        for (int i=0;i<50;i++){
-//            user = new User();
-//            user.setUserName("No"+i);
-//            user.setPassword("123456");
-//            list.add(user);
-//        }
-//        userService.insert(list);
-//    }
-//
-//}
+package org.template.cloud.service.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.template.cloud.service.ServiceApplication;
+import org.template.cloud.service.domain.bean.mysql.User;
+import org.template.cloud.service.service.mysql.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceApplication.class)
+public class MysqlTest {
+    @Autowired
+    UserService userService;
+
+    public void userTest() {
+        User user = new User();
+        user.setUserName("admin");
+        user.setPassword("123456");
+        userService.insert(user);
+        List<User> list = userService.findAll();
+        System.out.println(list.size());
+    }
+
+    @Test
+    public void insertList() {
+        List<User> list = new ArrayList();
+        User user;
+        for (int i=0;i<50;i++){
+            user = new User();
+            user.setUserName("No"+i);
+            user.setPassword("123456");
+            list.add(user);
+        }
+        userService.insert(list);
+    }
+
+}
