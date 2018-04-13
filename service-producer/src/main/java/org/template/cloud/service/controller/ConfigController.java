82567d9504781d63fa8d8888d170e34228e1d28e
liaoyuecai <liaoyuecai@163.com>
2018-04-13 14:36:41 +0800

    bus

--- service-producer/src/main/java/org/template/cloud/service/controller/ConfigController.java
+++ service-producer/src/main/java/org/template/cloud/service/controller/ConfigController.java
@@ -1,20 +1,21 @@
 package org.template.cloud.service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.template.cloud.service.deploy.CloudTestConfig;
 
 @RestController
 @RefreshScope
 public class ConfigController {
 
-    CloudTestConfig Config;
+    @Value("${test.name}")
+    String test;
 
     @RequestMapping("/getTest")
     @ResponseBody
     public String getTest() {
-        return Config.name();
+        return test;
     }
 }
