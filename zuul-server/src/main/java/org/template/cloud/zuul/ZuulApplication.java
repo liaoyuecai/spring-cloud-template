9aa31faa9b575ed405a3fa8482a8e586d4995fed
liaoyuecai <liaoyuecai@163.com>
2018-06-07 15:31:16 +0800

    跨域配置

--- zuul-server/src/main/java/org/template/cloud/zuul/ZuulApplication.java
+++ zuul-server/src/main/java/org/template/cloud/zuul/ZuulApplication.java
@@ -5,11 +5,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableZuulProxy
 @SpringCloudApplication
 @EnableDiscoveryClient
 public class ZuulApplication {
+    /**
+     * 跨域Cors配置
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
     }
